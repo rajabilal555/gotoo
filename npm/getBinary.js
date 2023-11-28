3e05a00212d72aeb8cec9ac8233d9f3c076ff882
Bilal Raja <rajabilal555@gmail.com>
2023-11-28 21:53:38 +0500

    release: build 0.1.8 Update npm install strategy

--- npm/getBinary.js
+++ npm/getBinary.js
@@ -1,36 +1,29 @@
 const { Binary } = require("binary-install");
 const os = require("os");
+const { join } = require("path");
+const packageInfo = require("../package.json");
 
 function getPlatform() {
   const type = os.type();
   const arch = os.arch();
 
-  if (type === "Windows_NT") {
-    if (arch === "x64") {
-      return "win64";
-    } else {
-      return "win32";
-    }
-  }
-
-  if (type === "Linux" && arch === "x64") {
-    return "linux";
-  }
-
-  if (type === "Darwin" && arch === "x64") {
-    return "macos";
-  }
+  if (type === "Windows_NT" && arch === "x64") return "win64";
+  if (type === "Windows_NT") return "win32";
+  if (type === "Linux" && arch === "x64") return "linux";
+  if (type === "Darwin" && arch === "x64") return "macos";
 
   throw new Error(
-    `Unsupported platform: ${type} ${arch}. Please create an issue at https://github.com/rajabilal555/gotoo/issues`
+    `Unsupported platform: ${type} ${arch}. Please create an issue at ${packageInfo.bugs.url}`
   );
 }
 
 function getBinary() {
   const platform = getPlatform();
-  const version = require("../package.json").version;
+  const version = packageInfo.version;
   const url = `https://github.com/rajabilal555/gotoo/releases/download/v${version}/gotoo-${platform}.tar.gz`;
-  return new Binary("gotoo", url);
+  return new Binary("gotoo", url, version, {
+    installDirectory: join(__dirname),
+  });
 }
 
 module.exports = getBinary;
